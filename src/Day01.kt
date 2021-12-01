e9c24cf4787fb508add69a6aaffe4f5c3b23cebc
Dominik Plewa <dominik.plewa@allegro.pl>
2021-12-01 13:37:58 +0100

    Add solution day 1, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,29 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(depths: List<String>): Int {
+        var previousDepth = Int.MAX_VALUE
+        var depthIncrements = 0
+
+        depths.map { it.toInt() }.forEach {
+            if (previousDepth < it) {
+                depthIncrements++
+            }
+            previousDepth = it
+        }
+
+        return depthIncrements
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
+    // TEST part 1
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
+    // RESULT part 1
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("PART 1: ${part1(input)}")
+
+    println("PART 2: ${part2(input)}")
 }
